fabf372789b09c802f054f6adc370886d2174ff1
Johnny B <typngeek@aim.com>
2020-02-12 23:02:57 -0500

    add more helpful error text

--- src/colors.js
+++ src/colors.js
@@ -7,7 +7,7 @@ function init() {
     if (!output || !input) {
         // check if either input or output connection failed
         statusText.innerHTML = "Connection failed."
-        messageText.innerHTML = "Please connect the Launchpad and ensure you have the correct drivers."
+        messageText.innerHTML = "Please connect the Launchpad and ensure you have the correct <a href='https://customer.novationmusic.com/en/support/downloads?brand=Novation&product_by_range=481&download_type=all'>drivers</a>."
     } else {
         statusText.innerHTML = "Connected to Novation Launchpad MK2."
         initLaunchpadColors(output);
@@ -23,7 +23,7 @@ function init() {
 
 WebMidi.enable(function (err) {
     if (err) {
-        console.log("WebMidi could not be enabled.", err);
+        statusText.innerHTML = "This browser does not support WebMIDI. Try using a browser listed <a href='https://caniuse.com/#feat=midi'>here</a>."
     }
 
     init(); // run init once
